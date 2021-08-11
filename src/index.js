35c64483fb72bc8c42e73c72ef381a006fb3d356
vikmaamaa <maamaavictor@yahoo.com>
2021-08-11 19:34:36 +0100

    first

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import {AppProvider} from './context'
+ReactDOM.render(
+  <React.StrictMode>
+      <AppProvider> <App /></AppProvider>
+   
+  </React.StrictMode>,
+  document.getElementById('root')
+)
